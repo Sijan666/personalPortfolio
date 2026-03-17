61d39900a31c6f755dd299462bdb41463f23f5ff
Sijan666 <666majharulislam@gmail.com>
2026-03-17 22:14:14 +0600

    feat : add banner section

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -19,11 +19,11 @@ const Banner = () => {
                             <Button btnText={'View Projects'} className={'px-5 py-2 bg-[#2B7FFE] text-white text-sm rounded-md'}/>
                             <div className="h-9 w-1 bg-white/10"></div>
                             <div className="socialIcons flex gap-x-5 items-center text-white/50">
-                                <FaGithub size={20}/>
-                                <FaLinkedin size={20}/>
-                                <FaFacebook size={20}/>
-                                <GrInstagram size={20}/>
-                                <FaDiscord size={20}/>
+                                <FaGithub size={20} className="cursor-pointer"/>
+                                <FaLinkedin size={20} className="cursor-pointer"/>
+                                <FaFacebook size={20} className="cursor-pointer"/>
+                                <GrInstagram size={20} className="cursor-pointer"/>
+                                <FaDiscord size={20} className="cursor-pointer"/>
                             </div>
                         </div>
                     </div>
