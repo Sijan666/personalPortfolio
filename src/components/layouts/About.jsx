7208b7c5edfca2d5c1dd15f3e3f3d988eee5a1cb
Sijan666 <666majharulislam@gmail.com>
2026-03-18 20:43:23 +0600

    add commenting in about section

--- src/components/layouts/About.jsx
+++ src/components/layouts/About.jsx
@@ -11,26 +11,30 @@ const About = () => {
         <section id='about'>
             <div className="py-10 bg-[#0A0F1C] font-geist">
                 <Container>
+                    {/* heading text */}
                     <div className="heading">
                         <h3 className='text-[36px] font-bold text-[#EDEDED] beforeAfter'>About Me</h3>
                     </div>
                     <div className="mt-[72px]">
                         <div className="flex justify-between">
+                            {/* left side */}
                             <div className="left lg:w-[816px]">
+                                {/* 1st chapter */}
                                 <div className="bio flex gap-x-3">
                                     <div className="p-[9px] h-10 w-10 border border-[#2B7FFE] rounded-md ">
                                         <LuFileSpreadsheet size={20} className='text-[#2B7FFE]' />
                                     </div>
-                                    <div className="">
+                                    <div className="text">
                                         <h4 className='text-[18px] font-medium text-[#2B7FFE]'>Bio</h4>
                                         <p className='text-white/60 text-base pt-3.5'>Frontend Developer with 1+ year of experience building responsive and scalable web applications using React and Next.js. Skilled in turning UI/UX designs into pixel-perfect, high-performing code, with a strong focus on clean architecture, performance optimization, and modern frontend practices.</p>
                                     </div>
                                 </div>
-                                <div className="bio flex gap-x-3 mt-10">
+                                {/* 2nd chapter */}
+                                <div className="experience flex gap-x-3 mt-10">
                                     <div className="p-[9px] h-10 w-10 border border-[#2B7FFE] rounded-md ">
                                         <PiBagSimple size={20} className='text-[#2B7FFE]' />
                                     </div>
-                                    <div className="">
+                                    <div className="text">
                                         <h4 className='text-[18px] font-medium text-[#2B7FFE]'>Experience</h4>
                                         <h4 className='text-[#EDEDED] text-base pt-3.5'>Front-end Developer (Intern)</h4>
                                         <p className='pt-1 text-white/60 text-base'>Creative IT</p>
@@ -38,13 +42,17 @@ const About = () => {
                                     </div>
                                 </div>
                             </div>
+                            {/* right side */}
                             <div className="right">
+                                {/* img */}
                                 <Images imgSrc={sijan}/>
+                                {/* details */}
                                 <div className="mt-5">
                                     <h4 className='text-[20px] font-medium text-[#EDEDED]'>Majharul Islam</h4>
                                     <p className='pt-1 text-white/60 text-sm'>Frontend Developer (React.js & Next.js)</p>
                                     <p className='pt-1 text-white/60 text-sm'>Creating High-Performance, Modern Web Applications</p>
                                 </div>
+                                {/* btn */}
                                 <div className="btn mt-5">
                                     <button className='py-2 px-3 bg-[#2B7FFE] text-white text-sm font-medium rounded-md flex items-center gap-x-2 cursor-pointer'><MdOutlineFileDownload size={20}/>Download Resume</button>
                                 </div>
