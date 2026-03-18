18c43d98668d5887cabff43a3c11ab3f14260094
Sijan666 <666majharulislam@gmail.com>
2026-03-18 21:12:01 +0600

    add responsive and commenting in banner section

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -9,16 +9,19 @@ const Banner = () => {
     return (
         <>
         <section id="banner">
-            <div className="py-25 bg-[#0A0F1C] font-geist">
+            <div className="py-15 lg:py-25 bg-[#0A0F1C] font-geist">
                 <Container>
                     <div className="text-center">
-                        <h1 className="text-[72px] font-bold text-[#EDEDED]">A Fabulous</h1>
-                        <h2 className="text-[72px] font-bold text-[#FE9A00]">Frontend Developer</h2>
-                        <p className="py-10 text-[#EDEDED] text-base leading-6 lg:w-[685px] mx-auto">Hi, My name is Md. Nuruzzaman, and I am a Frontend developer based in Dhaka, Bangladesh . I specialize in building modern, responsive web applications.</p>
-                        <div className="flex justify-center gap-x-8">
+                        {/* heading */}
+                        <h1 className="text-[30px] lg:text-[72px] font-bold text-[#EDEDED]">A Fabulous</h1>
+                        <h2 className="text-[30px] lg:text-[72px] font-bold text-[#FE9A00]">Frontend Developer</h2>
+                        <p className="py-10 text-[#EDEDED] text-sm lg:text-base leading-6 lg:w-[685px] mx-auto">Hi, My name is Majharul Islam, and I am a Frontend developer from Bangladesh . I specialize in building modern, responsive web applications.</p>
+                        <div className="flex flex-col lg:flex-row justify-center gap-y-8 gap-x-8">
+                            {/* btn */}
                             <Button btnText={'View Projects'} className={'px-5 py-2 bg-[#2B7FFE] text-white text-sm rounded-md'}/>
-                            <div className="h-9 w-1 bg-white/10"></div>
-                            <div className="socialIcons flex gap-x-5 items-center text-white/50">
+                            <div className="h-9 w-1 bg-white/10 hidden lg:block"></div>
+                            {/* social links */}
+                            <div className="socialIcons flex lg:gap-x-5 gap-x-8 items-center justify-center text-white/50">
                                 <FaGithub size={20} className="cursor-pointer"/>
                                 <FaLinkedin size={20} className="cursor-pointer"/>
                                 <FaFacebook size={20} className="cursor-pointer"/>
