4e0c7ceaaa513420ee5b96830776b1d13f1ac445
Sijan666 <666majharulislam@gmail.com>
2026-03-18 21:05:29 +0600

    add responsive in header section

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,19 +1,29 @@
 import Container from "../Container"
 import Images from "../Images"
 import logo from '../../assets/logo.png'
+import { useState } from "react"
+import { FaBarsStaggered } from "react-icons/fa6"
 
 
 const Header = () => {
+
+    const [show,setShow] = useState(false)
+
+    const handleShow = ()=>{
+        setShow(!show)
+    }
+
+
     return (
         <>
         <header>
             <div className="py-10 bg-[#0A0F1C] font-geist">
                 <Container>
                     <div className="flex justify-between items-center">
-                        <div className="logo w-[10%]">
+                        <div className="logo lg:w-[10%] w-[25%]">
                             <Images imgSrc={logo}/>
                         </div>
-                        <nav className="lg:w-[37%] place-items-end">
+                        <nav className="lg:w-[37%] place-items-end hidden md:block">
                             <ul className="flex gap-x-5 text-white ">
                                 <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">Home</li>
                                 <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">About Me</li>
@@ -22,6 +32,21 @@ const Header = () => {
                                 <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">Contact</li>
                             </ul>
                         </nav>
+                        {/* mobile display */}
+                        <div className="mobileNav md:hidden">
+                            <FaBarsStaggered onClick={handleShow} size={20} className="text-white relative"/>
+                            {show && (
+                            <nav className="lg:w-[37%] absolute top-20 left-1/2 -translate-x-1/2 w-[90%] bg-white rounded-md p-5">
+                                <ul className="flex flex-col lg:flex-row gap-x-5 text-white items-center gap-y-5 justify-center overflow-hidden">
+                                    <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">Home</li>
+                                    <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">About Me</li>
+                                    <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">Tech Stack</li>
+                                    <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">Projects</li>
+                                    <li className="text-[#99A1AF] text-base font-medium leading-6 cursor-pointer">Contact</li>
+                                </ul>
+                            </nav>
+                            )}
+                        </div>
                     </div>
                 </Container>
             </div>
