56f03ea9edb6231bfa9236b4a115b2a64d93e872
Sijan666 <666majharulislam@gmail.com>
2026-03-20 21:30:22 +0600

    add 5 project

--- src/components/data/projectSApi.js
+++ src/components/data/projectSApi.js
@@ -2,6 +2,8 @@ import { tech1, tech2, tech3, tech4, tech5 } from "./teckStack";
 import p1 from '../../assets/p1.png'
 import orebi from '../../assets/orebi.png'
 import port from '../../assets/port.png'
+import eduact from '../../assets/eduact.png'
+import creative from '../../assets/creative.png'
 
 export const allProjects = [
     {
@@ -26,23 +28,23 @@ export const allProjects = [
     },
     {
         id : 3 ,
-        projectImg: p1,
-        projectName:"Library Hub ",
-        projectType:'Personal Project',
-        projectDetails:'A web application to manage library operations including book cataloging, member registration, borrowing/return tracking, and overdue fine management.',
+        projectImg: eduact,
+        projectName:"EduAct",
+        projectType:'Team Project',
+        projectDetails:'An interactive e-learning platform designed to provide online courses, study resources, and a seamless learning experience.',
         techStack:tech3,
-        liveLink:'https://main-portfolio-two-khaki.vercel.app/',
-        githubLink:'https://github.com/Sijan666/mainPortfolio',
+        liveLink:'https://edu-act-figma.vercel.app/',
+        githubLink:'https://github.com/Sijan666/eduActFigma',
     },
     {
         id : 4 ,
-        projectImg: p1,
-        projectName:"Brand Shop",
-        projectType:'Personal Project',
+        projectImg: creative,
+        projectName:"Creative IT ( Web & Software )",
+        projectType:'Team Project',
         projectDetails:'A modern car brand website featuring product showcases, model comparisons, booking test drives, and dealership locator.',
         techStack:tech4,
-        liveLink:'https://main-portfolio-two-khaki.vercel.app/',
-        githubLink:'https://github.com/Sijan666/mainPortfolio',
+        liveLink:'https://creative-web-six.vercel.app/',
+        githubLink:'https://github.com/Sijan666/creativeWeb',
     },
     {
         id : 5 ,
