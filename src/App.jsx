10c636adc24d80ba0bf8e77f4e2c211f188d0d7c
Sijan666 <666majharulislam@gmail.com>
2026-03-20 22:05:48 +0600

    add lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
+import { Routes , Route } from 'react-router-dom'
+import { ReactLenis } from 'lenis/react' 
 
 import Home from './components/pages/Home'
-import { Routes , Route } from 'react-router-dom'
 import RootLayouts from './components/layouts/RootLayouts'
 import Error from './components/pages/Error'
 
-
 function App () {
   return (
-    <>
+    <ReactLenis 
+      root 
+      options={{
+        lerp: 0.05,
+        duration: 1.5,
+        smoothWheel: true, 
+        wheelMultiplier: 0.8,
+      }}
+    >
       <Routes>
         <Route path="/" element={<RootLayouts />}>
           <Route index element={<Home/>} />
         </Route>
-          <Route path="*" element={<Error/>} />
+        <Route path="*" element={<Error/>} />
       </Routes>
-    </>
+    </ReactLenis>
   )
 }
 
 export default App
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { BrowserRouter } from 'react-router-dom'
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css' 
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
