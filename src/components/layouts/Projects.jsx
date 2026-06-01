f2737ef59e344600a7c2d0e718e61696ea795135
Sijan666 <666majharulislam@gmail.com>
2026-06-02 00:58:38 +0600

    add btn in projects

--- src/components/layouts/Projects.jsx
+++ src/components/layouts/Projects.jsx
@@ -3,7 +3,6 @@ import Container from '../Container'
 import { allProjects } from '../data/projectSApi'
 import Project from '../Project'
 
-
 const Projects = () => {
     return (
         <>
@@ -15,6 +14,7 @@ const Projects = () => {
                         <h3 className='lg:text-[36px] text-[30px] font-bold text-[#EDEDED] beforeAfter'>Projects</h3>
                         <p className='text-base text-[#99A1AF] lg:w-[559px] leading-6 pt-8'>Explore some of the personal and team-based projects I have built showcasing my skills and expertise.</p>
                     </div>
+                    
                     <div className="mt-10">
                         <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 items-center gap-5">
                             {allProjects.map((item)=>(
@@ -31,6 +31,12 @@ const Projects = () => {
                                 </div>
                             ))}
                         </div>
+                    </div>
+                    {/* btn */}
+                    <div className="flex justify-center mt-12">
+                        <a href="https://github.com/sijan666" target="_blank" className="cursor-pointer px-8 py-3 text-sm font-medium tracking-wide text-[#EDEDED] border border-[#374151] rounded-lg hover:bg-[#1F2937] transition-all duration-300 inline-block">
+                            View More Projects
+                        </a>
                     </div>
                 </Container>
             </div>
