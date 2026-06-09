6ba92552d02cf9a7db92ac9be6ac6762556d5290
Sijan666 <666majharulislam@gmail.com>
2026-06-10 01:45:23 +0600

    update banner

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -28,7 +28,9 @@ const Banner = () => {
                         <p className="lg:py-10 py-6 text-[#EDEDED] text-sm lg:text-base leading-6 lg:w-[420px] mx-auto">A passionate Front-End Developer from Bangladesh . Always curious to learn, explore and collaborate.</p>
                         <div className="flex flex-col lg:flex-row justify-center gap-y-8 gap-x-8">
                             {/* btn */}
-                            <a href="https://github.com/Sijan666" target="_blank"><Button btnText={'View Projects'} className={'px-5 py-2 bg-[#2B7FFE] text-white text-sm rounded-md'}/></a>
+                            <a href="https://github.com/sijan666" target="_blank" className="cursor-pointer px-5 py-2 text-sm font-medium tracking-wide text-[#EDEDED] border border-[#374151] rounded-md hover:bg-[#1F2937] transition-all duration-300 inline-block">
+                                View Projects
+                            </a>
                             <div className="h-9 w-1 bg-white/10 hidden lg:block"></div>
                             {/* social links */}
                             <div className="socialIcons flex lg:gap-x-5 gap-x-8 items-center justify-center text-white/50">
