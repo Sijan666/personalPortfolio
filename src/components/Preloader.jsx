a46389cf3c2f6ffcc3683857d8e501cb24057b26
Sijan666 <666majharulislam@gmail.com>
2026-06-11 15:11:05 +0600

    fix issues in preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -26,32 +26,21 @@ const Preloader = ({ onComplete }) => {
     }, [progress, onComplete]);
 
     return (
-        <div
-        className={`fixed inset-0 z-9999 flex items-center justify-center bg-[#070707] transition-all duration-[1.2s] ease-[cubic-bezier(0.76,0,0.24,1)] ${
+        <div className={`fixed inset-0 z-9999 flex items-center justify-center bg-[#070707] transition-all duration-[1.2s] ease-[cubic-bezier(0.76,0,0.24,1)] ${
             stage === "hideAll"
             ? "opacity-0 backdrop-blur-md pointer-events-none scale-105"
-            : "opacity-100 scale-100"
-        }`}>
-        <div
-            className={`flex flex-col items-center transition-opacity duration-700 ease-in-out ${
-            stage !== "loading"
-                ? "opacity-0 translate-y-2"
-                : "opacity-100 translate-y-0"
-            }`}
-        >
-            {/* Extremely Minimalist Typography */}
-            <h2 className="text-gray-300 text-[11px] md:text-xs font-light uppercase tracking-[0.5em] mb-5 ml-[0.5em]">
-            Majharul Islam
-            </h2>
-
-            {/* Ultra-thin Elegant Progress Line */}
-            <div className="w-40 md:w-56 h-px bg-gray-800/50 overflow-hidden">
-            <div
-                className="h-full bg-linear-to-r from-orange-500/40 to-orange-500 transition-all duration-300 ease-linear"
-                style={{ width: `${progress}%` }}
-            />
+            : "opacity-100 scale-100"}`}>
+            <div className={`flex flex-col items-center transition-opacity duration-700 ease-in-out ${
+                stage !== "loading"
+                    ? "opacity-0 translate-y-2"
+                    : "opacity-100 translate-y-0"}`}>
+                <h2 className="text-gray-300 text-[11px] md:text-xs font-light uppercase tracking-[0.5em] mb-5 ml-[0.5em]">
+                    Majharul Islam
+                </h2>
+                <div className="w-40 md:w-56 h-px bg-gray-800/50 overflow-hidden">
+                    <div className="h-full bg-linear-to-r from-orange-500/40 to-orange-500 transition-all duration-300 ease-linear" style={{ width: `${progress}%` }}/>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
